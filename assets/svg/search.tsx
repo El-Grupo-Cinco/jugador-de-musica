36a1267b28d5458cac93839d343263c37256794f
eckofox1981 <eckofox1981@pm.me>
2025-09-25 13:02:57 +0200

    chore: add search svg

---
+++ assets/svg/search.tsx
@@ -0,0 +1,18 @@
+import Svg, { G, Path } from "react-native-svg";
+
+export function Search({ size = 24, color = "white" }) {
+  return (
+    <Svg width={size} height={size} viewBox="0 0 16 16" fill="none">
+      <G id="Interface / Search_Magnifying_Glass">
+        <Path
+          id="Vector"
+          d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z"
+          stroke={color}
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </G>
+    </Svg>
+  );
+}
