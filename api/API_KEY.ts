25893409227550cbb83b42cf3693d356c30c99ef
eckofox1981 <eckofox1981@pm.me>
2025-09-30 11:02:36 +0200

    feat: add API_KEYS file and searchByQuery (untested)

---
+++ api/API_KEY.ts
@@ -0,0 +1,4 @@
+export const API_KEY: string = "Token 9g5q57EXR3lwMTlciXPI2Ojx7ZHIpamiHhXe1o0r";
+export const SEARCH_URL: string =
+  "https://freesound.org/apiv2/search/text/?query=";
+export const SOUND_URL: string = "https://freesound.org/apiv2/sounds/";
---
+++ api/searchByQuery.ts
@@ -0,0 +1,55 @@
+import { Sound } from "@/objets/sound";
+import { API_KEY, SEARCH_URL } from "./API_KEY";
+
+export async function searchByQuery(query: string, api_key: string) {
+  try {
+    const response = await fetch(`${SEARCH_URL}${query}`, {
+      method: "GET",
+      headers: {
+        Authorization: API_KEY,
+      },
+    });
+    if (!response.ok) {
+      const message: string = await response.text();
+      throw new Error(message);
+    }
+
+    const json = await response.json();
+
+    if (!json.results || json.results === 0) {
+      throw new Error("No sound result returned.");
+    }
+
+    const soundList = json.results.map(
+      (sound: {
+        id: string;
+        url: string;
+        name: string;
+        tags: String[];
+        description: string;
+        category: string;
+        duration: number;
+        created: string;
+        username: string;
+        images: object;
+      }) =>
+        new Sound(
+          sound.id,
+          sound.url,
+          sound.name,
+          sound.tags,
+          sound.description,
+          sound.category,
+          sound.duration,
+          sound.created,
+          sound.username,
+          sound.images
+        )
+    );
+
+    return soundList;
+  } catch (error) {
+    //TODO implement elegant solution
+    console.log(error.message);
+  }
+}
