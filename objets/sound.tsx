d86f018874dc05fa5b2346078e7118c3b6f0880e
eckofox1981 <eckofox1981@pm.me>
2025-09-30 10:36:17 +0200

    feat: add Sound class with constructor

---
+++ objets/sound.tsx
@@ -0,0 +1,36 @@
+export class Sound {
+  id: string;
+  url: string;
+  name: string;
+  tags: Array<String>;
+  description: string;
+  category: string;
+  duration: number;
+  created: string;
+  username: string;
+  images: object;
+
+  constructor(
+    id: string,
+    url: string,
+    name: string,
+    tags: Array<String>,
+    description: string,
+    category: string,
+    duration: number,
+    created: string,
+    username: string,
+    images: object
+  ) {
+    this.id = id;
+    this.url = url;
+    this.name = name;
+    this.tags = tags;
+    this.description = description;
+    this.category = category;
+    this.duration = duration;
+    this.created = created;
+    this.username = username;
+    this.images = images;
+  }
+}
