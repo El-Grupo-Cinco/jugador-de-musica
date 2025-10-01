4677b9dab7ff35d01ffe3f8c8e3dd676cd0e169c
itsabunny <itsabunny@pm.me>
2025-10-01 11:30:51 +0200

    fix: replace Text component with ThemedText in SearchPage

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,7 +1,5 @@
 import { ThemedText } from "@/components/themed-text";
-import { Text } from "react-native";
 
 export default function SearchPage() {
-  // return <Text>Search page here</Text>;        <-- text is black
   return <ThemedText>Search page here</ThemedText>
 }
