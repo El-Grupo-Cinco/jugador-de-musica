b12d2004e6bbc90f11863f4a7b040809b5fa6524
itsabunny <itsabunny@pm.me>
2025-10-01 11:31:32 +0200

    fix: remove unused imports and commented code in SongPage

--- app/(tabs)/[sound].tsx
+++ app/(tabs)/[sound].tsx
@@ -1,9 +1,5 @@
 import { ThemedText } from "@/components/themed-text";
-import { useThemeColor } from "@/hooks/use-theme-color";
-import { Text } from "react-native";
 
 export default function SongPage() {
-  //const textColor = useThemeColor({}, "text")
-  //return <Text style={{color:textColor}}>Music player here</Text>;
   return <ThemedText>Music player here</ThemedText>
 }
