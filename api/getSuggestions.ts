8043eb7e6288a45720d6b975678d5813cb1dcdc6
eckofox1981 <eckofox1981@pm.me>
2025-10-02 10:17:36 +0200

    feat: add getSuggestion.ts

---
+++ api/getSuggestions.ts
@@ -0,0 +1,45 @@
+import { SoundSearchResult } from "@/objects/searchResult";
+import { API_KEY, SEARCH_URL } from "./API_KEY";
+
+export async function getSuggestions() {
+  try {
+    const response = await fetch(`${SEARCH_URL}piano&page_size=5`, {
+      method: "GET",
+      headers: {
+        Authorization: API_KEY,
+      },
+    });
+    if (!response.ok) {
+      const message: string = await response.text();
+      throw new Error(message);
+    }
+
+    const json = await response.json();
+
+    if (!json.results || json.results === 0) {
+      throw new Error("No sound result returned.");
+    }
+
+    const soundResultList = json.results.map(
+      (sound: {
+        id: string;
+        name: string;
+        tags: string[];
+        license: string;
+        username: string;
+      }) =>
+        new SoundSearchResult(
+          sound.id,
+          sound.name,
+          sound.tags,
+          sound.license,
+          sound.username
+        )
+    );
+
+    return soundResultList;
+  } catch (error) {
+    //TODO implement elegant solution
+    console.log(error instanceof Error ? error.message : String(error));
+  }
+}
