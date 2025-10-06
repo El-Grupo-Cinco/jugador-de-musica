34d2e195fcc41dbb4aed0a813a4351d620134610
ZeverJ <zever96@hotmail.com>
2025-10-06 14:30:10 +0200

    Added SearchBar
    SearchBar has been added on the Search Page.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import SearchBar from '@/components/searchbar';
+import { useQueryStore } from '@/store/store';
 import { ThemedText } from "@/components/themed-text";
 
 export default function SearchPage() {
-  return <ThemedText>Search page here</ThemedText>
+    const query = useQueryStore((state) => state.query);
+    const setQuery = useQueryStore((state) => state.setQuery);
+
+    return (
+        <View style={{ padding: 20 }}>
+            <SearchBar query={query} setQuery={setQuery} />
+            <ThemedText>Search page here</ThemedText>
+        </View>
+    );
 }
