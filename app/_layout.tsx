63d643943a2650a811a9283d8d1011d235521c34
eckofox1981 <eckofox1981@pm.me>
2025-10-17 10:07:18 +0200

    refactor: remove unsused modal stack link

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,10 +32,6 @@ export default function RootLayout() {
             ),
           }}
         />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
