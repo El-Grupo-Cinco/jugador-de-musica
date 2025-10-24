7d4689765f4b0d2f70f5f84d182c75d814cb88fb
eckofox1981 <eckofox1981@pm.me>
2025-10-24 13:54:06 +0200

    feat: update API key fetching, add API_KEY.ts to git ignore

--- api/API_KEY.ts
+++ api/API_KEY.ts
@@ -1,4 +1,6 @@
-export const API_KEY: string = "Token YOUR_API_KEY_GOES_HERE";
+import { FREESOUND_API_KEY } from "@/API_KEY";
+
+export const API_KEY: string = `Token ${FREESOUND_API_KEY}`;
 export const SEARCH_URL: string =
   "https://freesound.org/apiv2/search/text/?query=";
 export const SOUND_URL: string = "https://freesound.org/apiv2/sounds/";
